2ef7e65c060e665251c701c82a187589c0ab421a
Luis Amorim <luis@amorim.bio.br>
2025-02-21 09:05:26 -0300

    Initial Commit

---
+++ firebaseAuth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  firebaseAuth
+//
+//  Created by Luis Amorim on 21/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ firebaseAuth/firebaseAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  firebaseAuthApp.swift
+//  firebaseAuth
+//
+//  Created by Luis Amorim on 21/02/25.
+//
+
+import SwiftUI
+
+@main
+struct firebaseAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
